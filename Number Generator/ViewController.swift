9d9a8b13b6ec7c72cd7ecd95bf5ccc596a3d9c31
Hygor Costa <cost0142@algonquinlive.com>
2022-05-26 12:03:02 -0400

    Initial Commit

---
+++ Number Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Number Generator
+//
+//  Created by Hygor Costa on 2022-05-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
